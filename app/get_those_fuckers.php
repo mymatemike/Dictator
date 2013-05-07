5e95db95daf8aada1e45296dead7814f00c76d3f
Mike <mike@hollersydney.com.au>
2013-05-07 11:46:01 +1000

    Added Slug Detection

--- app/get_those_fuckers.php
+++ app/get_those_fuckers.php
@@ -1,30 +1,41 @@
 <?php
-	require "app/db/db_connect.php";
-	require "utils/string_functions.php";
+require "app/db/db_connect.php";
+require "utils/string_functions.php";
 
-	$dbconnect = NEW DB_Class('when_I_am_dictator', 'root', 'root');
+$dbconnect = NEW DB_Class('when_I_am_dictator', 'root', 'root');
+
+if(isset($_GET['slug'])){
+
+	//do what they asked
+	$query = "SELECT *  FROM wiad_fuckers WHERE is_live = 1 AND slug = \"" . mysql_real_escape_string($_GET['slug']) . "\" limit 0,1";
+
+}else{
+
 	$query = "SELECT *  FROM wiad_fuckers WHERE is_live = 1 ORDER BY RAND() limit 0,1";
-	$results = $dbconnect->fetch($query);
+}
+$results = $dbconnect->fetch($query);
 
-	foreach ($results as $value){
+foreach ($results as $value){
 		//print_r($value);
 		//var_dump($value);
-		$thatfucker = $value['the_fucker'];
+	$thatfucker = $value['the_fucker'];
 
-		$the_fucker = $value['the_fucker'];
-  		$the_fucker = ucfirst(strtolower($the_fucker));
+	$the_fucker = $value['the_fucker'];
+	$the_fucker = ucfirst(strtolower($the_fucker));
 
-  		$fucker_link = $value['fucker_link'];
-		if (is_null($fucker_link) || $fucker_link == ""){
-			$str = finishyourfuckingsentences($the_fucker);
-			$str = strtotitle($str);
+	$fucker_link = $value['fucker_link'];
+	if (is_null($fucker_link) || $fucker_link == ""){
+		$str = finishyourfuckingsentences($the_fucker);
+		$str = strtotitle($str);
 			//$str .= "id='thedictator' Mike Hill";
-			echo $str;
-		}else{
-			$str = finishyourfuckingsentences($the_fucker);
-			$str = strtotitle($str);
-			$str = "<a href= '$fucker_link' target='_blank'>" . $str . "</a>";
-			echo $str;
-		}
-  	}
+		echo $str;
+	}else{
+		$str = finishyourfuckingsentences($the_fucker);
+		$str = strtotitle($str);
+		$str = "<a href= '$fucker_link' target='_blank'>" . $str . "</a>";
+		echo $str;
+	}
+}
+
+
 ?>
